80c8c9acb9a799a501882ca3eba898db8f54d7a2
Jack Everson <jack.everson0@gmail.com>
2026-06-21 17:12:53 +1000

    Added nlohmann/json.hpp

--- RoundAndRipe/GardenScene.hpp
+++ RoundAndRipe/GardenScene.hpp
@@ -15,6 +15,7 @@
 #include "glm/ext/vector_float2.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/geometric.hpp"
+#include <json.hpp>
 
 #include <cstddef>
 #include <string>
@@ -120,6 +121,11 @@ public:
   void onEnter(GLFWwindow &window) override {
     glfwSetInputMode(&window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     m_controller.Init(window);
+
+    nlohmann::json j;
+    j["hello"] = "hi";
+
+    std::string test = j["hello"];
 
     sound_manager.Initialize();
     sound_manager.LoadSound("background_noise",
